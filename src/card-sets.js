3c3f4cd8e65425c5e0448e306cf6701a48d2a6fe
Amen Kemal <57805638+mike964@users.noreply.github.com>
2022-07-11 19:18:25 +0430

    fruit added

--- src/card-sets.js
+++ src/card-sets.js
@@ -11,7 +11,7 @@ export const flags = [
 	},
 	{ src: process.env.PUBLIC_URL + '/assets/flags/Austria.png' },
 	{
-		src: process.env.PUBLIC_URL + '/assets/flags/SouthKorea.png',
+		src: process.env.PUBLIC_URL + '/assets/flags/Korea.png',
 	},
 	{ src: process.env.PUBLIC_URL + '/assets/flags/Denmark.png' },
 	{ src: process.env.PUBLIC_URL + '/assets/flags/Belgium.png' },
@@ -24,8 +24,44 @@ export const flags = [
 	{ src: process.env.PUBLIC_URL + '/assets/flags/America.png' },
 	{ src: process.env.PUBLIC_URL + '/assets/flags/Norway.png' },
 	{ src: process.env.PUBLIC_URL + '/assets/flags/Malta.png' },
+	{ src: process.env.PUBLIC_URL + '/assets/flags/Qatar.png' },
+	{ src: process.env.PUBLIC_URL + '/assets/flags/Bahrain.png' },
+	{ src: process.env.PUBLIC_URL + '/assets/flags/Oman.png' },
+	{ src: process.env.PUBLIC_URL + '/assets/flags/Turkey.png' },
+	{ src: process.env.PUBLIC_URL + '/assets/flags/Lebanon.png' },
+	{ src: process.env.PUBLIC_URL + '/assets/flags/India.png' },
+	{ src: process.env.PUBLIC_URL + '/assets/flags/Tunisia.png' },
+	{ src: process.env.PUBLIC_URL + '/assets/flags/Argentina.png' },
 ]
 
-export const fruits = ['Apple', 'Orange']
+export const fruits = [
+	{
+		src: process.env.PUBLIC_URL + '/assets/fruit/apple.png',
+	},
+	{
+		src: process.env.PUBLIC_URL + '/assets/fruit/orange.png',
+	},
+	{
+		src: process.env.PUBLIC_URL + '/assets/fruit/banana.png',
+	},
+	{
+		src: process.env.PUBLIC_URL + '/assets/fruit/pineapple.png',
+	},
+	{
+		src: process.env.PUBLIC_URL + '/assets/fruit/watermelon.png',
+	},
+	{
+		src: process.env.PUBLIC_URL + '/asssets/fruit/pomegranate.png',
+	},
+	{
+		src: process.env.PUBLIC_URL + '/assets/fruit/strawberry.png',
+	},
+	{
+		src: process.env.PUBLIC_URL + '/assets/fruit/cherry.png',
+	},
+	{
+		src: process.env.PUBLIC_URL + '/assets/fruit/grape.png',
+	},
+]
 
 export const teams = ['Arsenal', 'Chelsea']
